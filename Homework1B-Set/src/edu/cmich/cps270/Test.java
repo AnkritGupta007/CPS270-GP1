f0539480daa6f5b376d15bcd105f1eca75bf4181
Samuel St John <stjoh1sr@cmich.edu>
2021-01-29 09:10:49 -0500

    Fixing bit-shifting issues

---
+++ Homework1B-Set/src/edu/cmich/cps270/Test.java
@@ -0,0 +1,20 @@
+// TODO: Delete
+
+package edu.cmich.cps270;
+import java.util.*;
+
+public class Test {
+
+	public static void main(String[] args) throws SetElementValueOutOfRange{
+		// TODO: REMOVE
+			List<Integer> listy = new ArrayList<Integer>();
+			Set s = new Set(listy);
+			
+			s.addElement(62);
+			s.addElement(2);
+			s.removeElement(62);
+			
+			System.out.println(s);
+		}
+
+	}
